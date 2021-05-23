e22aad1f8d5956ddd5426b6032caa7a3441891f2
ahmni <therinodanceshappy@gmail.com>
2021-05-23 12:56:53 -0500

    adding todo editing and removing projects functionality

--- script.js
+++ script.js
@@ -66,8 +66,8 @@ function runDefault() {
         let test = new todo ('test');
         let test2 = new todo ('work out');
         const defaults = createProject('Default', 'NA', 'blue', [test, test2]);
-        projects.push(defaults);
-        let currentProject = defaults;
+        projects.push(defaults); 
+        currentProject = defaults;
         addProjects('defaults');
         addProjectbtn('Default', defaults);
         loadProject(defaults);
@@ -81,7 +81,7 @@ Todos.addEventListener('click', () => {
     if (entry.value != '') {
         currentProject.addTodo(entry.value);
         entry.value = '';
-        localStorage.setItem("projects", JSON.stringify(projects)); 
+        autoSave();
     }
 });
 
@@ -113,6 +113,18 @@ function addTodos(text, project, completed, index) {
     newTodo.classList.add("todo-item");
     todoDiv.appendChild(newTodo);
 
+    const editTodo = document.createElement("box-icon");
+    editTodo.setAttribute("name", "edit");
+    editTodo.setAttribute("type", "solid");
+    editTodo.classList.add("edit");
+    todoDiv.appendChild(editTodo);
+
+    editTodo.addEventListener('click', () => {
+        todoFormContainer.style.display = 'flex';
+        todoForm(project, index, newTodo)
+        console.log(newTodo)
+    })
+
     const deleteTodo = document.createElement("button");
     deleteTodo.classList.add("delete") 
     deleteTodo.innerText = 'X';
@@ -134,7 +146,7 @@ function addTodos(text, project, completed, index) {
     deleteTodo.addEventListener('click', () => {
         todoDiv.parentNode.removeChild(todoDiv);
         project.list.splice(index, 1);
-        localStorage.setItem("projects", JSON.stringify(projects)); 
+        autoSave();
     })
 
     if (project) {
@@ -143,14 +155,70 @@ function addTodos(text, project, completed, index) {
         }
     }
 }
-let formContainer = document.querySelector('#form');
+let projectFormContainer = document.querySelector('#project-form');
+let todoFormContainer = document.querySelector('#todo-form');
 
-function createForm() {
+function todoForm(project, index, newTodo) {
+    let form;
+    newTodo.classList.add('select')
+    function edits(name, description, dueDate, notes) {
+        let curTodo = document.querySelector('.select') 
+        curTodo.innerText = name;
+        project.list[index].title = name;
+        project.list[index].descrption = description;
+        project.list[index].dueDate = dueDate;
+        project.list[index].notes = notes;
+        curTodo.classList.remove('select')
+        autoSave();
+    }
+
+    function createForm() {
+        form = document.createElement("form"); //create's the submission form
+        createtodoInput(form, 'tName');
+        createtodoInput(form, 'tDescription');
+        createtodoInput(form, 'Due Date');
+        createtodoInput(form, 'Notes');
+        //form.style.display = 'none';
+
+        const submit = document.createElement('button');
+        submit.type = 'button';
+        submit.classList.add('submit');
+        submit.classList.add('button2');
+        submit.innerHTML = 'Add';
+        form.appendChild(submit);
+        //submit.style.display = 'none';
+        submit.addEventListener('click', () => {
+            let name = document.getElementById("tName").value;
+            let description = document.getElementById('tDescription').value;
+            let dueDate = document.getElementById('Due Date').value;
+            let notes = document.getElementById('Notes');
+            console.log(name, description);
+            inputs.forEach((x) => {
+                x.value = '';
+            })
+    
+            if (name == '' || description == '') {
+                alert('Please fill out author and title.')
+            } else {
+                edits(name, description, dueDate, notes);
+                closeForm(todoFormContainer);
+            }
+        })
+    }
+    form = document.querySelector('#tName')
+    
+    if (!form) {
+        createForm()
+    }
+
+    const inputs = document.querySelectorAll('input');
+}
+
+function createProjectForm() {
     let form = document.createElement("form"); //create's the submission form
     createInput(form, 'Name');
     createInput(form, 'Description');
-    createInput(form, 'Due Date');
-    createInput(form, 'Completed');
+    createInput(form, 'Color');
     //form.style.display = 'none';
 
     const submit = document.createElement('button');
@@ -160,45 +228,39 @@ function createForm() {
     submit.innerHTML = 'Add';
     form.appendChild(submit);
     //submit.style.display = 'none';
-
+    
     submit.addEventListener('click', () => {
         let name = document.getElementById("Name").value;
         let description = document.getElementById('Description').value;
-        let dueDate = document.getElementById('Due Date').value;
-        let completed = document.getElementById('Completed');
-        let status;
+        let dueDate = document.getElementById('Color').value;
         inputs.forEach((x) => {
             x.value = '';
         })
-        if (completed.checked) {
-            status = 'on';
-        } else {
-            status = 'off';
-        }
         if (name == '' || description == '') {
-            alert('Please fill out author and title.')
+            alert('Please fill out naem and description.')
         } else {
             let newProject = createProject(name, description, dueDate, []);
             addProjectbtn(name, newProject);
             projects.push(newProject);
-            localStorage.setItem("projects", JSON.stringify(projects)); 
-            closeForm();
+            autoSave();
+            closeForm(projectFormContainer);
         }
     })
 
     const inputs = document.querySelectorAll('input');
 }
 
+
 function addProjects(text) {
     const btn = document.querySelector('.addProject');
     
     btn.addEventListener('click', () => {
-        formContainer.style.display = 'flex';
-        console.log('yeet');
+        projectFormContainer.style.display = 'flex';
     });
 }
 
-createForm();
+
+createProjectForm();
 addProjects();
 
 function addProjectbtn(name, project) {
@@ -216,12 +278,27 @@ function addProjectbtn(name, project) {
             loadProject(project);
         }
     });
+    let mouseIsDown = false;
+    newProject.addEventListener('mousedown', function(event) { 
+        mouseIsDown = true
+        // simulating hold event
+        setTimeout(function() {
+          if(mouseIsDown) {
+            console.log('holding')
+            let index = projects.findIndex(x => x.name = name)
+            if (currentProject == projects[index]) {
+                while (list.firstChild) {
+                    list.removeChild(list.firstChild)
+                }
+            }
+            projects.splice(index, 1)
+            projectContainer.removeChild(newProject)
+            autoSave()
+          }
+        }, 2000);
+      });
+    newProject.addEventListener('mouseup', () => {mouseIsDown = false})
 }
-
-
-
-
-
 
 function createInput(f, labels) {
     let label = document.createElement("div");
@@ -244,14 +321,41 @@ function createInput(f, labels) {
     display.appendChild(f);
 }
 
+function createtodoInput(f, labels) {
+    let label = document.createElement("div");
+    label.classList.add('label');
+    label.innerHTML = labels;
+    
+    let input = document.createElement("input"); //input element, text
+    input.setAttribute('type',"text");
+    input.setAttribute('id', labels);
+    if (labels == 'Completed') {
+        label.innerHTML = 'Completed?';
+        input.setAttribute('type', 'checkbox');
+    }
+
+    f.appendChild(label);
+    f.appendChild(input);
+
+    let display = document.getElementById('formcontent1')
+    //display.style.display = 'none';
+    display.appendChild(f);
+}
+
 const close = document.querySelector('#close');
 
 close.addEventListener('click', e => {
-    closeForm();
+    closeForm(projectFormContainer);
 });
 
-function closeForm() {
-    formContainer.style.display = 'none';
+const close1 = document.querySelector('#close1');
+
+close1.addEventListener('click', e => {
+    closeForm(todoFormContainer);
+})
+
+function closeForm(container) {
+    container.style.display = 'none';
 }
 
 
@@ -288,4 +392,8 @@ function getProjects() {
             projects.push(cur);
         });
     }
+}
+
+function autoSave() {
+    localStorage.setItem("projects", JSON.stringify(projects)); 
 }
